910082fcd309432be95970a3361644fee2217032
nga20167304 <hocmetmuonchet@gmail.com>
2019-11-11 21:18:42 +0700

    create database

---
+++ database/migrations/2019_10_15_100636_create_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id_job');
+            $table->integer('id_com');
+            $table->string('name_job');
+            $table->date('deadline');
+            $table->string('content');
+            $table->integer('salary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
---
+++ database/migrations/2019_10_15_101104_create_apply_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apply', function (Blueprint $table) {
+            $table->integer('id_std');
+            $table->integer('id_job');
+            $table->enum('status',['completed','uncompleted','progressing','searching']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apply');
+    }
+}
